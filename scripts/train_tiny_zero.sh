b019a41004da872a442edd03940cafa61e60c207
SaltedFish23 <2641106027@qq.com>
2025-01-26 00:17:40 +0800

    run

--- scripts/train_tiny_zero.sh
+++ scripts/train_tiny_zero.sh
@@ -1,8 +1,18 @@
+export N_GPUS=1
+export BASE_MODEL=~/autodl-tmp/models/qwen2.5-0.5B
+export DATA_DIR=/root/autodl-tmp/datas/countdown1
+export ROLLOUT_TP_SIZE=1
+export EXPERIMENT_NAME=countdown-qwen2.5-0.5b
+export VLLM_ATTENTION_BACKEND=XFORMERS
+export WANDB_API_KEY=9d9bba64d50f78261f1b0d884b8e1dddd22479fa
+
+
+
 python3 -m verl.trainer.main_ppo \
 data.train_files=$DATA_DIR/train.parquet \
 data.val_files=$DATA_DIR/test.parquet \
-data.train_batch_size=256 \
-data.val_batch_size=1312 \
+data.train_batch_size=32 \
+data.val_batch_size=256 \
 data.max_prompt_length=256 \
 data.max_response_length=1024 \
 actor_rollout_ref.model.path=$BASE_MODEL \
@@ -11,14 +21,16 @@ actor_rollout_ref.actor.ppo_mini_batch_size=128 \
 actor_rollout_ref.actor.ppo_micro_batch_size=8 \
 actor_rollout_ref.rollout.log_prob_micro_batch_size=8 \
 actor_rollout_ref.rollout.tensor_model_parallel_size=$ROLLOUT_TP_SIZE \
-actor_rollout_ref.rollout.gpu_memory_utilization=0.4 \
+actor_rollout_ref.rollout.gpu_memory_utilization=0.8 \
 actor_rollout_ref.ref.log_prob_micro_batch_size=4 \
 critic.optim.lr=1e-5 \
 critic.model.path=$BASE_MODEL \
 critic.ppo_micro_batch_size=8 \
 algorithm.kl_ctrl.kl_coef=0.001 \
++algorithm.gradient_accumulation_steps=8 \
 trainer.logger=['wandb'] \
 +trainer.val_before_train=False \
++trainer.precision=bf16 \
 trainer.default_hdfs_dir=null \
 trainer.n_gpus_per_node=$N_GPUS \
 trainer.nnodes=1 \
